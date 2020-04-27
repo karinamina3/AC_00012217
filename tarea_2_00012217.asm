50bfa4845f8a3ded179df007975b65f3390e58b9
Karina Mina <37252764+karinamina3@users.noreply.github.com>
2020-04-26 23:18:54 -0600

    ejercicio 1

--- tarea_2_00012217.asm
+++ tarea_2_00012217.asm
@@ -1,1 +1,21 @@
+        org 100h
 
+; -- Ejercicio 1
+; Últimos 5 dígitos de mi carnet: 1 2 2 1 7
+; Promedio: 2.6, aprox 2 - Comentario: 'Aún se pasa'
+
+section .text 
+        MOV di, 0d
+        MOV cx, [length]
+
+eje1:   MOV bl, [commentary+di]
+        MOV [di+200h], bl
+        INC di
+        LOOP eje1
+
+section .data
+        commentary db  'Aun se pasa' 
+        length  equ $-commentary
+
+
+        int 20h
