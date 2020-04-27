756f574e3a2d0ad22ac02062b62baf7016311abe
Karina Mina <37252764+karinamina3@users.noreply.github.com>
2020-04-26 23:23:11 -0600

    ejercicio 2

--- tarea_2_00012217.asm
+++ tarea_2_00012217.asm
@@ -18,4 +18,28 @@ section .data
         length  equ $-commentary
 
 
+; -- Ejercicio 2: Bayib Nukele
+
+        MOV ax, 0000h
+        MOV bx, 0000h
+        MOV cx, 0000h
+        MOV cx, 2d
+        MOV ax, 4d
+        MOV bx, 0h
+        MOV [210h], ax
+
+eje:    INC bx
+        MUL cx
+        MOV [210h + bx], ax
+        CMP ax, 0XFF
+        JB eje
+
+eje2:   INC bx
+        INC bx
+        MUL cx
+        MOV [210h + bx], ax
+        CMP bx, 12d
+        JBE eje2
+
+
         int 20h
